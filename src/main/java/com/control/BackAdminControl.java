720f153224b542247c2c058fdfe0867bd69f816d
chengliao <chengliaofight@163.com>
2018-01-19 17:08:54 +0800

    对导入模板进行了格式输入要求

--- src/main/java/com/control/BackAdminControl.java
+++ src/main/java/com/control/BackAdminControl.java
@@ -14,10 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.poi.hssf.usermodel.DVConstraint;
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDataValidation;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.util.CellRangeAddressList;
 import org.json.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -101,6 +104,12 @@ public class BackAdminControl {
 	    {  
 	        HSSFWorkbook workbook = new HSSFWorkbook();  
 	        HSSFSheet sheet = workbook.createSheet("导入模板");  
+	        String[] sex = { "男","女" };    
+	        String[] role = { "成果上传者","成果审核员","系统管理员" }; 
+	        String[] position = { "教授","副教授","助理教授","讲师" }; 
+	        sheet = setHSSFValidation(sheet, sex, 0, 500, 1, 1);// 第一列的前501行都设置为选择列表形式. 
+	        sheet = setHSSFValidation(sheet, role, 0, 500, 2, 2);// 第一列的前501行都设置为选择列表形式. 
+	        sheet = setHSSFValidation(sheet, position, 0, 500, 3, 3);// 第一列的前501行都设置为选择列表形式. 
 	        HSSFRow row= sheet.createRow((short)0);  
 	        HSSFCell cell = null; 
 	        //写入各个字段的名称  
@@ -118,6 +127,61 @@ public class BackAdminControl {
 	        cell.setCellValue("工号");
 	        return workbook;  
 	    }  
+	
+	
+	
+	public static HSSFSheet setHSSFValidation(HSSFSheet sheet,    
+            String[] textlist, int firstRow, int endRow, int firstCol,    
+            int endCol) {    
+        // 加载下拉列表内容    
+        DVConstraint constraint = DVConstraint    
+                .createExplicitListConstraint(textlist);    
+        // 设置数据有效性加载在哪个单元格上,四个参数分别是：起始行、终止行、起始列、终止列    
+        CellRangeAddressList regions = new CellRangeAddressList(firstRow,    
+                endRow, firstCol, endCol);    
+        // 数据有效性对象    
+        HSSFDataValidation data_validation_list = new HSSFDataValidation(    
+                regions, constraint);    
+        sheet.addValidationData(data_validation_list);    
+        return sheet;    
+    }    
+    
+    /**  
+     * 设置单元格上提示  
+     *   
+     * @param sheet  
+     *            要设置的sheet.  
+     * @param promptTitle  
+     *            标题  
+     * @param promptContent  
+     *            内容  
+     * @param firstRow  
+     *            开始行  
+     * @param endRow  
+     *            结束行  
+     * @param firstCol  
+     *            开始列  
+     * @param endCol  
+     *            结束列  
+     * @return 设置好的sheet.  
+     */    
+    public static HSSFSheet setHSSFPrompt(HSSFSheet sheet, String promptTitle,    
+            String promptContent, int firstRow, int endRow, int firstCol,    
+            int endCol) {    
+        // 构造constraint对象    
+        DVConstraint constraint = DVConstraint    
+                .createCustomFormulaConstraint("BB1");    
+        // 四个参数分别是：起始行、终止行、起始列、终止列    
+        CellRangeAddressList regions = new CellRangeAddressList(firstRow,    
+                endRow, firstCol, endCol);    
+        // 数据有效性对象    
+        HSSFDataValidation data_validation_view = new HSSFDataValidation(    
+                regions, constraint);    
+        data_validation_view.createPromptBox(promptTitle, promptContent);    
+        sheet.addValidationData(data_validation_view);    
+        return sheet;    
+    }    
+    
 	//excel导入,导入的过程有点长，所以使用异步快比较一些
 	@RequestMapping(value="/back/admin/importExcel" ,method=RequestMethod.POST)
 	public  Callable<String> importExcel( final MultipartFile file){
